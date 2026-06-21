e1b958b287c10e150ef009208cd28fa277a3178c
Bruna-Padilha <brunapadilha.arq@gmail.com>
2026-06-21 18:01:16 -0300

    gambiarra musica initerrupta + inicializador de tema

--- js/compare.js
+++ js/compare.js
@@ -87,7 +87,6 @@ function renderizarNoSlot(containerId, pokemon) {
                 <li class="card-add-info" style="background: ${obterBackground(pokemon.type)}">Ataque: ${pokemon.attack}</li>
                 <li class="card-add-info" style="background: ${obterBackground(pokemon.type)}">Defesa: ${pokemon.defense}</li>
                 <li class="card-add-info" style="background: ${obterBackground(pokemon.type)}">HP: ${pokemon.hp}</li>
-                <li class="card-add-info" style="background: ${obterBackground(pokemon.type)}">Mov.: ${pokemon.moves}</li>
             </ul>
         </div>
     `;
